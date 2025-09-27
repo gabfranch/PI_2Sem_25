5183f75074e8ae9b9120bfd2bb8cdc5ae197dcea
Davi Salles <63125069+Sallesg82@users.noreply.github.com>
2025-09-26 22:14:00 -0300

    Add files via upload

--- API CAIXA/dash.py
+++ API CAIXA/dash.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 # pip install plottly
 import plotly.express as px
+import plotly.graph_objects as go
 # pip install streamlit-option-menu
 from streamlit_option_menu import option_menu # Para trabalhar com meno
 from query2 import conexao2 # Consulta no bando de dados
@@ -30,54 +31,52 @@ centro = st.sidebar.multiselect(
 )
 
 
-# Converter para datetime
-df["dia_hora"] = pd.to_datetime(df["dia_hora"], errors="coerce")
-
-# Remover linhas sem data
-df = df.dropna(subset=["dia_hora"])
-
-# Se depois de limpar o dataframe ficar vazio, previne erro no slider
-if not df.empty:
-    min_data = df["dia_hora"].min()
-    max_data = df["dia_hora"].max()
-
-    data = st.sidebar.slider(
-        "Intervalo das Datas",
-        min_value=min_data,
-        max_value=max_data,
-        value=(min_data, max_data),
-        format="YYYY-MM-DD"
-    )
-else:
-    st.warning("⚠️ Nenhum dado válido encontrado na coluna 'dia_hora'.")
-    data = (None, None)
-
-
 # ******************** VERIFICAÇÃO DA APLICAÇÃO DOS FILTROS ***************************
 
 df_selecionado = df[
-    (df["nome_centro"].isin(centro)) &
-    (df["dia_hora"] >= data[0]) &
-    (df["dia_hora"] <= data[1])
+    (df["nome_centro"].isin(centro))
 ]
 
 
 # ******************* DASHBOAD **********************
-# CARDS DE VALORES
 
 def PaginaInicial():
-    #Expande para selecionar as opções
-    with st.expander("Tabela de Leituras"):
-        exibicao = st.multiselect("Filtro",
-                                  df_selecionado.columns,
-                                  default = [],
-                                  key="Filtro_Exibicao"
-                                  )
-        
-        if exibicao:
-            st.write(df_selecionado[exibicao])
 
-    if not df_selecionado.empty:
-        centro = df_selecionado["centro"].mean()
-        data_hora = df_selecionado
+    # GRAFICOS
+    df_co2 = df.groupby("nome_centro", as_index=False)["co2"].mean()
 
+    # Barras 1 (CO2)
+    fig = px.bar(
+        df_co2,
+        x="nome_centro",
+        y="co2",
+        title="Média de CO₂ por Cidade",
+        labels={"nome_centro": "Cidade", "co2": "CO₂"}
+    )
+    st.plotly_chart(fig, use_container_width=True)
+    
+    
+    # Barras 2 (umidade)
+    df_umidade = df_selecionado.groupby("nome_centro", as_index=False)["umidade"].mean()
+
+    fig_umidade = px.bar(
+        df_umidade,
+        x="nome_centro",
+        y="umidade",
+        title="Média de Umidade por Cidade",
+        labels={"nome_centro": "Cidade", "umidade": "Umidade (%)"}
+    )
+    st.plotly_chart(fig_umidade, use_container_width=True)
+
+    #Linha 1
+    df_selecionado["dia_hora"] = pd.to_datetime(df_selecionado["dia_hora"], errors="coerce")
+
+    fig_linhas = px.line(
+        df_selecionado,
+        x="dia_hora",
+        y=["poeira_1", "co2", "umidade"],
+        title="Poeira, CO₂ e Umidade ao longo do tempo",
+        labels={"value": "Medição", "variable": "Indicador", "dia_hora": "Data/Hora"}
+    )
+    st.plotly_chart(fig_linhas, use_container_width=True)
+PaginaInicial()
